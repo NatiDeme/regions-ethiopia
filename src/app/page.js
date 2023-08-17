edb269da18eb32493e5b51334def355341c47c3c
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-08-17 14:41:10 +0300

    Add data and create dynamic buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
+"use client";
 import Image from "next/image";
+import Modal from "./components/Modal";
+import { useState } from "react";
 import Indicators from "./components/indicators";
+import data from "./data/ethiopian_region_isolated.json";
 
 export default function Home() {
+  console.log(data.maps[0]);
+  const [content, setContent] = useState();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleClick = (id) => {
+    const country = data.maps.find((x) => x.map === id);
+    setModalOpen(true);
+    setContent(country);
+  };
   return (
     <>
-      <Indicators bgprimary="bg-black" borderprimary="border-black" />
+      {modalOpen && <Modal setOpenModal={setModalOpen} contents={content} />}
+      {data.maps.map((city, i) => (
+        <button id={`${i}`} key={i} onClick={() => handleClick(city.map)}>
+          {" "}
+          <Indicators bgprimary="bg-black" borderprimary="border-black" />
+        </button>
+      ))}
     </>
   );
 }
