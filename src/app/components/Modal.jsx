0e00b9d25b88b6104804465d323d33c454b5af15
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-08-19 11:01:43 +0300

    Add styling to the card

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 const Modal = ({ contents }) => {
   return (
     <>
-      <div className="inline-block mt-3 text-center">
-        <h1 className="text-2xl">{contents.title}</h1>
+      <div className="inline-block text-center">
+        <h1 className="text-2xl font-bold tracking-tight  text-gray-900">
+          {contents.title}
+        </h1>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="flex items-center justify-center col-span-2 text-base text-center ">
-          <p className="">{contents.description}</p>
-        </div>
-        <div>
-          <Image
-            src={contents.image_path}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
-        </div>
+      <p className="flex-1 mb-3 leading-tight text-sm font-normal text-gray-700 ">
+        {contents.description}
+      </p>
+      <div>
+        <Image
+          src={contents.image_path}
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
       </div>
       <div className="flex-[20%] flex justify-center items-center">
         {contents.title}
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Modal from "./components/Modal";
 import Draggable from "react-draggable";
 import { useState } from "react";
 import Indicators from "./components/indicators";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import data from "./data/ethiopian_region_isolated.json";
 
 export default function Home() {
@@ -34,15 +35,14 @@ export default function Home() {
           scale={1}
         >
           <div className="handle">
-            <div className="w-[50rem] max-h-min mt-10 rounded-xl flex flex-col p-6 bg-white shadow-[0_5px_15px_rgba(0,0,0,0.35)] ">
-              <div className="w-full flex justify-end">
-                <button
-                  className="text-2xl bg-transparent border-none cursor-pointer"
-                  onClick={() => handleClose()}
-                >
-                  X
-                </button>
-              </div>
+            <div className="relative w-[40rem] min-h-[30rem] overflow-scroll overflow-x-hidden max-h-min rounded-xl flex flex-col p-6 bg-white shadow-[0_5px_15px_rgba(0,0,0,0.35)] ">
+              <button
+                className="absolute top-2 right-2 w-min text-2xl bg-transparent border-none cursor-pointer"
+                onClick={() => handleClose()}
+              >
+                <XMarkIcon className="h-6 w-6 text-black" />
+              </button>
+
               <Modal contents={content} />
             </div>
           </div>
