4a1dfb074841ee3b27b43cc102f2453a675f710d
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-08-27 23:23:28 +0300

    Fix draggable and image full screen view

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -1,26 +1,104 @@
 import Image from "next/image";
-const Modal = ({ contents }) => {
+import Draggable from "react-draggable";
+import React, { useState } from "react";
+import { XMarkIcon, EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+
+const Modal = ({ contents, setShowDraggable }) => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+
+  const openImageModal = (index) => {
+    setSelectedImageIndex(index);
+  };
+  const handleClose = () => {
+    setShowDraggable(false);
+  };
+  const closeImageModal = () => {
+    setSelectedImageIndex(null);
+  };
   return (
     <>
-      <div className="inline-block text-center">
-        <h1 className="text-2xl font-bold tracking-tight  text-gray-900">
-          {contents.title}
-        </h1>
-      </div>
-      <p className=" mb-3 leading-tight text-sm font-normal text-gray-700 ">
-        {contents.description}
-      </p>
-      <div>
-        <Image
-          src={contents.image_path}
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-      </div>
-      <div className="flex-[10%] flex justify-center items-center">
-        {contents.title}
-      </div>
+      <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        grid={[5, 5]}
+        bounds="parent"
+        scale={1}
+      >
+        <div className=" zoom-in absolute top-0 right-0 z-10">
+          <div className="relative resize w-[30rem] h-[40rem] overflow-x-hidden max-h-min rounded-xl flex flex-col bg-neutral-600 shadow-[0_5px_15px_rgba(0,0,0,0.35)] ">
+            <div className="flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+              <button className="handle box-content w-4 h-4 p-1 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline cursor-grab active:cursor-grabbing">
+                <EllipsisVerticalIcon className="h-6 w-6 text-white" />
+              </button>
+              <h5
+                className="text-xl font-medium leading-normal text-white"
+                id="exampleModalScrollableLabel"
+              >
+                {contents.title}
+              </h5>
+              <button
+                onClick={() => handleClose()}
+                className="box-content w-4 h-4 p-1 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline"
+              >
+                <XMarkIcon className="h-6 w-6 text-white" />
+              </button>
+            </div>
+            <div className="flex-auto overflow-y-auto relative p-4">
+              <div className="inline-block text-center">
+                <h1 className="text-2xl font-bold tracking-tight text-white">
+                  {contents.title}
+                </h1>
+              </div>
+              <p className=" mb-3 leading-tight text-sm font-normal text-white">
+                {contents.description}
+              </p>
+              <div className="flex flex-wrap gap-4 justify-around">
+                {contents.images.map((img, i) => (
+                  <div
+                    key={i}
+                    className="cursor-pointer"
+                    onClick={() => openImageModal(i)}
+                  >
+                    <Image
+                      className="h-auto rounded-lg bg-white"
+                      src={img.image_path}
+                      width={300}
+                      height={300}
+                      alt="Picture of the author"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+              <button
+                onClick={() => handleClose()}
+                className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </Draggable>
+      {selectedImageIndex !== null && (
+        <div className="fixed top-0 right-0 z-50 w-screen h-screen bg-black flex justify-center items-center">
+          <Image
+            className="max-h-full max-w-full"
+            width={600}
+            height={600}
+            src={contents.images[selectedImageIndex].image_path}
+            alt="Full-screen"
+          />
+          <button
+            className="absolute top-4 right-4 text-white"
+            onClick={closeImageModal}
+          >
+            Close
+          </button>
+        </div>
+      )}
     </>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -9,53 +9,20 @@ import data from "./data/ethiopian_region_isolated.json";
 
 export default function Home() {
   const [content, setContent] = useState(data.maps[0]);
-  const [modal, setModal] = useState(false);
   const [showDraggable, setShowDraggable] = useState(false);
   // const [isDragging, setIsDragging] = useState(false);
-  // console.log(content);
+  console.log(content);
 
   const toggleDraggable = (id) => {
     const country = data.maps.find((x) => x.map === id);
     setContent(country);
     setShowDraggable(!showDraggable);
   };
-
-  const handleClose = () => {
-    // setIsDragging(true);
-    setShowDraggable(false);
-  };
-
-  // const handleDragStop = () => {
-  //   setIsDragging(false);
-  // };
   return (
     <>
       <div className="relative h-screen">
         {showDraggable && (
-          <Draggable
-            axis="both"
-            handle=".handle"
-            defaultPosition={{ x: 0, y: 0 }}
-            grid={[5, 5]}
-            bounds="parent"
-            scale={1}
-          >
-            <div className=" zoom-in absolute top-0 right-0 z-10">
-              <div className="relative resize w-[40rem] h-[29rem] overflow-scroll overflow-x-hidden max-h-min rounded-xl flex flex-col p-6 bg-white shadow-[0_5px_15px_rgba(0,0,0,0.35)] ">
-                <button
-                  className="absolute top-2 right-2 w-min text-2xl bg-transparent border-none cursor-pointer"
-                  onClick={() => handleClose()}
-                >
-                  <XMarkIcon className="h-6 w-6 text-black" />
-                </button>
-                <button className="handle absolute top-2 left-2 w-min text-2xl bg-transparent border-none cursor-pointer">
-                  <EllipsisVerticalIcon className="h-6 w-6 text-black" />
-                </button>
-
-                <Modal className contents={content} />
-              </div>
-            </div>
-          </Draggable>
+          <Modal setShowDraggable={setShowDraggable} contents={content} />
         )}
         {data.maps.map((city, i) => (
           <button id={`${i}`} key={i} onClick={() => toggleDraggable(city.map)}>
